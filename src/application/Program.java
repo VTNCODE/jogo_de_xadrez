11d7a4fd8d061a894e388eb2d619a83ccaf23d04
Vitor Gulak <vitorayrtongulakdeandrade@gmail.com>
2025-08-04 15:20:00 -0300

    Classe position criada!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import boardgame.Position;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,7 +10,9 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hello world");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+
     }
 
 }
